08e6fcbf9408cfd92942298dfaf79b087a38de76
Marci0707 <schneider.marcell2@gmail.com>
2023-12-29 18:30:38 +0100

    download embeddings on setup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
+import os.path
+import subprocess
+
 import matplotlib
 import streamlit as st
 
+from consts import EMBEDDING_FILE
 from db import WordDB
 from page import build_page
 from word_maze_state import WordMazeState
@@ -19,14 +23,27 @@ def get_state() -> WordMazeState:
     return WordMazeState()
 
 
-def main():
+def download_embeddings():
+    with st.spinner(text="Downloading embeddings...(may take some minutes)", ):
+        logging.info('Downloading embeddings...')
+        subprocess.call(["wget", f"http://nlp.stanford.edu/data/{EMBEDDING_FILE.replace('.txt', '.zip')}"])
+    with st.spinner(text="Unzipping embeddings...", ):
+        logging.info('Unzipping embeddings...')
+        subprocess.call(["unzip", f"{EMBEDDING_FILE.replace('.txt', '.zip')}"])
+
+def setup():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S'
                         )
     st.set_page_config(layout="wide")
     st.title('WordMaze Playground')
+    if not os.path.exists(EMBEDDING_FILE):
+        download_embeddings()
 
+
+def main():
+    setup()
     build_page(get_state(), get_db())
 
 
